be37cf27e9ab87d874faa040b2ab926d92df7202
ForestDweller014 <39864689+ForestDweller014@users.noreply.github.com>
2018-06-23 23:07:57 -0400

    Update Usage.cs

--- Usage.cs
+++ Usage.cs
@@ -1,13 +1,17 @@
-Client.TcpSend(username);
-Client.sendDone.WaitOne();
-response = Client.TcpReceive();
-Client.receiveDone.WaitOne();
-if (response == "Correct username") {
-    Client.TcpSend(password);
-    Client.sendDone.WaitOne();
-    response = Client.TcpReceive();
-    Client.receiveDone.WaitOne();
-    Console.WriteLine(response);
-} else {
-    Console.WriteLine(response);
+public class Usage {
+    public static void Main() {
+        Client.TcpSend(username);
+        Client.sendDone.WaitOne();
+        response = Client.TcpReceive();
+        Client.receiveDone.WaitOne();
+        if (response == "Correct username") {
+            Client.TcpSend(password);
+            Client.sendDone.WaitOne();
+            response = Client.TcpReceive();
+            Client.receiveDone.WaitOne();
+            Console.WriteLine(response);
+        } else {
+            Console.WriteLine(response);
+        }
+    }
 }
